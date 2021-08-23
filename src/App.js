963ff75f8061311ba457f8c2b0440a99b5222331
Hanh <tran.hanh3012@gmail.com>
2021-08-23 22:26:35 +1000

    Complete styling and search function

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import { CardList } from "./components/card-list/card-list.component";
 
 import React, { Component } from "react";
 
+import { SearchBox } from "./components/search-box/search-box.component";
+
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -19,10 +22,24 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <h1> Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monster"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
